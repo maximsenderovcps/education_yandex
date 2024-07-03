8b20672db8fa9d116a4869568304add234a7523f
maximsenderovcps <maximsenderov@cps.ai>
2024-07-03 14:22:19 +0300

    add data.js, app

---
+++ src/components/app/app.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import {Pages} from "components/pages";
+import {ProductsContext} from "components/entities/products";
+
+import './app.module.css'
+
+
+function App() {
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(false)
+    const [isError, setError] = useState(false)
+
+    useEffect(()=>{
+        setLoading(true)
+        setError(false)
+
+        fetch('/utils/data.js')
+            .then((response)=>response.json())
+            .then((json)=>json || [])
+            .then((json)=>setData(json))
+            .catch((reason) => setError(true))
+            .finally(()=>setLoading(false))
+    }, [data])
+
+
+    return (
+        <ProductsContext.Provider value={data}>
+            <Pages/>
+        </ProductsContext.Provider>
+    );
+}
+
+export default App;
